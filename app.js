d6eca8391e4cd63104d4defacd552d94dc2e84c1
Salman <salzahir18@gmail.com>
2025-04-08 16:40:28 -0500

    Refactor(auth): Simplify Passport config and correct db import

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ require('dotenv').config();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const session = require("express-session");
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Session configuration
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
 }));
+
+// Passport configuration
+const passport = require('passport');
+require('./db/passport-config')(passport); 
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -24,12 +27,13 @@ app.use(passport.session());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Static files CSS styling
 const publicDir = path.join(__dirname, 'public');
 app.use(express.static(publicDir));
 
+// Routes
 const routes = require('./routes/routes');
 app.use('/', routes);
-
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
--- db/queries.js
+++ db/queries.js
@@ -1,47 +1,5 @@
-const passport = require('passport');
 const db = require('./pool');
 const bcrypt = require('bcrypt');
-const LocalStrategy = require('passport-local').Strategy;
-
-passport.use(
-
-    new LocalStrategy(async (username, password, done) => {
-
-        try {
-            const {rows} = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-            user = rows[0];
-        
-            if (!user) {
-                return done(null, false, { message: "Incorrect username." });
-            }
-        
-            const isMatch = await bcrypt.compare(password, user.password);
-            if (isMatch) {
-                return done(null, user);
-            } else {
-                return done(null, false, { message: "Incorrect password." });
-            }
-
-        } catch (error) {
-            return done(error);
-        }
-
-    })
-)
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
-        const user = rows[0];
-        done(null, user);
-    } catch (error) {
-        done(error);
-    }
-});
 
 async function signUserUp(username, password) {
     try {
@@ -53,7 +11,6 @@ async function signUserUp(username, password) {
         throw error;
     }
 }
-
 
 async function getMessages() {
     try {
